d041070b2382142e26407064f6fbe2f83ce4aede
guemues <cagil.guemues@desy.de>
2024-10-20 15:00:14 +0200

    Initial Commit for the source code

---
+++ content/Rants/cagils-rants.md
@@ -0,0 +1,23 @@
+Title: My First Rant
+Date: 2024-10-24
+Category: Rants
+
+I tried Jekyll for so long and I still didn't manage to make something meaningful. If this also doesn't work out, I'm going to lose my fucking mind. 
+
+Anyway here is a picture of me and my dogs. Neo on the left and Titan on my lap.
+
+![]({attach}images/me_titan_neo.jpg)
+
+I'm going to use this place to rant about things. Talk about the stuff I'm passinate about.
+
+
+Topics such as: 
+
+* What is the goal function of life? 
+* FPGAs
+* Solutions to Fermi Paradox and why we should expand inward
+* Importance of NATO
+* Things related to Turkey. (Might write this in Turkish tbh) 
+* Cannabis Cultivation
+
+I swear I'm fun at parties.
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+AUTHOR = 'Çağıl Gümüş'
+SITENAME = 'Abused Engineer'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("Desy Gitlab Profile (Place you can see what I code in open-source world)",
+     "https://gitlab.desy.de/cagil.guemues"),
+    ("Soundcloud (Place you can hear what I do in music)",
+     "https://soundcloud.com/cagilgumus/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
